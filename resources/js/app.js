15583035d85bc4c74a8cb247e3a7f3a018ac838e
Arman <ak682015@gmail.com>
2020-08-05 14:11:05 +0530

    Vue Filter Added

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import moment from 'moment';
 import { Form, HasError, AlertError } from 'vform';
 
 window.Form = Form;
@@ -34,6 +35,10 @@ const router = new VueRouter({
 
 Vue.filter('upText', function(text){
     return text.charAt(0).toUpperCase() + text.slice(1);
+});
+
+Vue.filter('myDate', function(created){
+    return moment(created).format('MMMM Do YYYY, h:mm a');
 });
 
 
